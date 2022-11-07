1ee65c83a0654a1400b0761962255cba7cdef775
nikolas szwargot <nszwargot@gmail.com>
2022-11-07 14:40:26 +0100

    Some changes to constructor

--- src/LettersMatrix.java
+++ src/LettersMatrix.java
@@ -1,21 +1,22 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class LettersMatrix {
     public String[] words = {"ana", "alka", "olka", "lena"};
 
-    public ArrayList<String> letters;
+    public List<Character> letters;
 
     public int[][] letterCounters;
 
     public LettersMatrix(){
-        letters = new ArrayList<String>();
         for (int i = 0; i < words.length; i++){
             for (int j = 0; j < words[i].length(); j++){
-                if (!letters.contains(words[i][j])){
-
+                if (!letters.contains(words[i].charAt(j))){
+                    letters.add(words[i].charAt(j));
                 }
             }
         }
+
+        letterCounters = new int[words.length][letters.size()];
 
     }
 };
